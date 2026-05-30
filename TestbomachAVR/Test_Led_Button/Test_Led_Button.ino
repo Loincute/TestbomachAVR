b9359974afaad4a7a7364bdd63b3590fec3f9a08
Loincute <longtakuku@gmail.com>
2026-05-30 15:35:11 +0700

    Add files via upload

---
+++ TestbomachAVR/Test_Led_Button/Test_Led_Button.ino
@@ -0,0 +1,33 @@
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+  DDRB |= (1<<PA5);     // LED output
+  DDRD &= ~(1<<PD2);    // Button input
+
+  PORTD |= (1<<PD2);    // Pull-up
+
+  uint8_t state = 0;
+
+  while(1)
+  {
+    if(!(PIND & (1<<PD2))) // Button pressed
+    {
+      _delay_ms(20);
+
+      if(!(PIND & (1<<PD2)))
+      {
+        state ^= 1;
+
+        if(state)
+           PORTB |= (1<<PB0);
+        else
+          PORTB &= ~(1<<PB0);
+
+           while(!(PIND & (1<<PD2)));
+      }
+    }
+  }
+}
