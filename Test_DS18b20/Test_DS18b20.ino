07984712bcf070cb4be6b6f107a750f00bb6182f
Loincute <longtakuku@gmail.com>
2026-06-04 02:32:54 +0700

    Update Test_DS18b20.ino

--- Test_DS18b20/Test_DS18b20.ino
+++ Test_DS18b20/Test_DS18b20.ino
@@ -1,33 +1,41 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// DS18B20 noi vao PB0
-#define ONE_WIRE_BUS PB0
+// DS18B20 nối vào chân PD6
+#define ONE_WIRE_BUS PIN_PD6   // Nếu lỗi, xem phần ghi chú bên dưới
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-void setup()
-{
-    Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
 
-    Serial.println("DS18B20 Test");
+  Serial.println("Test DS18B20 on PD6");
 
-    sensors.begin();
+  sensors.begin();
 
-    Serial.print("So cam bien: ");
-    Serial.println(sensors.getDeviceCount());
+  int deviceCount = sensors.getDeviceCount();
+  Serial.print("So cam bien tim thay: ");
+  Serial.println(deviceCount);
+
+  if (deviceCount == 0) {
+    Serial.println("Khong tim thay DS18B20. Kiem tra chan PD6, dien tro 4.7k, day DQ.");
+  }
 }
 
-void loop()
-{
-    sensors.requestTemperatures();
+void loop() {
+  sensors.requestTemperatures();
 
-    float tempC = sensors.getTempCByIndex(0);
+  float tempC = sensors.getTempCByIndex(0);
 
+  if (tempC == DEVICE_DISCONNECTED_C) {
+    Serial.println("Loi: Khong doc duoc DS18B20");
+  } else {
     Serial.print("Nhiet do: ");
     Serial.print(tempC);
-    Serial.println(" C");
+    Serial.println(" *C");
+  }
 
-    delay(1000);
+  delay(1000);
 }
